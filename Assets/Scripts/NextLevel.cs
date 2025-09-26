43ef03c59b4121dd1da133e1c22348eb9abe455e
Justin Liang <justinliang715@gmail.com>
2025-09-26 12:47:47 -0400

    Added Scene changing

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+
+    public int sceneBuildIndex;
+
+    // Level move zoned enter, if collider is a player
+    // Move game to another scene
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger Entered");
+
+        if(other.tag == "Player")
+        {
+            // Player entered, so move level
+            Debug.Log("Switching Scene to " + sceneBuildIndex);
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+}
