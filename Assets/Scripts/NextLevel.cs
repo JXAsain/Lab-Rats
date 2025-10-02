a2965d916ced99c635685136b801775eeb498dd1
Justin Liang <justinliang715@gmail.com>
2025-10-02 16:36:35 -0400

    Added Level Change animations
    touch cheese, wipes black circle arcoss scene

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -8,6 +8,10 @@ public class NextLevel : MonoBehaviour
 
     public int sceneBuildIndex;
 
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
     // Level move zoned enter, if collider is a player
     // Move game to another scene
 
@@ -18,10 +22,19 @@ public class NextLevel : MonoBehaviour
         if(other.tag == "Player")
         {
             // Player entered, so move level
-            Debug.Log("Switching Scene from " + SceneManager.GetActiveScene().name);
+            // Debug.Log("Switching Scene from " + SceneManager.GetActiveScene().name);
+
+            StartCoroutine(LoadLevel(sceneBuildIndex));
+
+        }
+
+        IEnumerator LoadLevel(int sceneBuildIndex)
+        {
+            transition.SetTrigger("Start");
+
+            yield return new WaitForSeconds(transitionTime);
 
             SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
-
         }
 
     }
